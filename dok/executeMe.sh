d092429341bb9d73fd4c1cb9e37cee51f669e145
doclorenzo <98409558+doclorenzo@users.noreply.github.com>
2022-04-18 11:59:16 +0200

    Delete executeMe.sh

--- dok/executeMe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker-compose up -d
-echo "wait 12 seconds for container to start up"
-sleep 30
-echo "done"
-docker exec -it dokInfluxUni bash -c "influx setup --force --username a --password aaaaaaaa --org a --bucket a --token aaa --name me"
-echo "finished"
